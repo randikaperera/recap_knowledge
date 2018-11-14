041dea69f6aed38d0369c5238cd1d2f316f4bad7
Randika <randika.perera@travisperkins.co.uk>
2018-11-14 10:53:14 +0530

    change main line master

--- src/main/java/TestHello.java
+++ src/main/java/TestHello.java
@@ -1,5 +1,5 @@
 public class TestHello {
     public static void main(String[] args) {
-        System.out.println("This is the test program");
+        System.out.println("This is the test program1");
     }
 }
