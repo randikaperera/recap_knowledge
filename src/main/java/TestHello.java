d93ee6de6b6498176d4e264c8028ed7b3a763121
Randika <randika.perera@travisperkins.co.uk>
2018-11-14 10:55:37 +0530

    change main line 1

--- src/main/java/TestHello.java
+++ src/main/java/TestHello.java
@@ -1,6 +1,6 @@
 public class TestHello {
     public static void main(String[] args) {
-        System.out.println("This is the test program");
-        System.out.println("new line 1");
+        System.out.println("This is the test program123");
+        System.out.println("new line 12");
     }
 }
