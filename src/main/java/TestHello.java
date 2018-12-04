48dbdbd3f3b998d40f3fee359ffc37bc85bca959
Randika <randika.perera@travisperkins.co.uk>
2018-12-04 12:39:49 +0530

    add changes to master

--- src/main/java/TestHello.java
+++ src/main/java/TestHello.java
@@ -2,5 +2,8 @@ public class TestHello {
     public static void main(String[] args) {
         System.out.println("This is the test program123");
         System.out.println("new line 12");
+        System.out.println("Hello people");
+        System.out.println("hello students");
+        System.out.println("hi");
     }
 }
