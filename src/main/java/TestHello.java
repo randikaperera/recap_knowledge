54051e58381e1ed4110760df5a3b52274a318fd1
randikaperera <perera.randika@gmail.com>
2018-12-07 15:15:49 +0530

    Update TestHello.java

--- src/main/java/TestHello.java
+++ src/main/java/TestHello.java
@@ -1,9 +1,9 @@
 public class TestHello {
     public static void main(String[] args) {
-        System.out.println("This is the test program123");
+        System.out.println("This is the test program1235");
         System.out.println("new line 12");
         System.out.println("Hello people");
         System.out.println("hello students");
-        System.out.println("hi this line is changed by randika perera 1");
+        System.out.println("hi this line is changed by randika perera 5");
     }
 }
