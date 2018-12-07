86e4ee5e8f5dbb349c2c95c44c18dd35b4e42d5a
Randika <randika.perera@travisperkins.co.uk>
2018-12-07 15:06:16 +0530

    add changes

--- src/main/java/TestHello.java
+++ src/main/java/TestHello.java
@@ -1,9 +1,9 @@
 public class TestHello {
     public static void main(String[] args) {
-        System.out.println("This is the test program123");
+        System.out.println("This is the test program1234");
         System.out.println("new line 12");
         System.out.println("Hello people");
-        System.out.println("hello students");
-        System.out.println("hi this line is changed by randika perera 1");
+        System.out.println("hello studentswew");
+        System.out.println("hi this line is changed by randika perera 4");
     }
 }
